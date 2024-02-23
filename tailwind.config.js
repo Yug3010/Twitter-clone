9631c498cd5d8b615e20ad3d8841aad53482b110
Yug3010 <76808047+Yug3010@users.noreply.github.com>
2024-02-22 20:16:41 -0500

    done
    committed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    screens:{
+      xsm:{'min':'300px','max':'400px'},
+      sm: {'min':'400px','max':'600px'},
+      md: {'min':'600px','max':'800px'},
+      lg: {'min':'800px','max':'1200px'},
+    },
     extend: {},
   },
   plugins: [],
